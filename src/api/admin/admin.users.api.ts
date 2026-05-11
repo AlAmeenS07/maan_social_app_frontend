69846869a841c8bff58e200bc98c030eff4471fc
Al Ameen S <alameensameen77@gmail.com>
2026-05-11 20:19:11 +0530

    refactor : added api routes in constants file

--- src/api/admin/admin.users.api.ts
+++ src/api/admin/admin.users.api.ts
@@ -1,14 +1,15 @@
 import axiosInstance from "../../config/axios"
+import { apis } from "../../utils/constants"
 
 const baseUrl = import.meta.env.VITE_USER_SERVICE_BASE_URL
 
 export const adminUsersApi = async<T>(params : T) => {
-    const res = await axiosInstance.get(baseUrl + "/admin/users", {params})
+    const res = await axiosInstance.get(baseUrl + apis.ADMIN_USERS , {params})
     return res.data
 }
 
 
 export const adminUserStatusApi = async(id : string) => {
-    const res = await axiosInstance.patch(baseUrl + `/admin/users/${id}`)
+    const res = await axiosInstance.patch(baseUrl + apis.ADMIN_USERS + `/${id}`)
     return res.data
 }
---
+++ src/utils/constants.ts
@@ -0,0 +1,16 @@
+
+export const apis = {
+    ADMIN_AUTH_LOGIN : "/admin/auth/login",
+    ADMIN_AUTH_LOGOUT : "/admin/auth/logout",
+    ADMIN_USERS : "/admin/users",
+    USER_AUTH_REGISTER : "/user/auth/register",
+    USER_AUTH_LOGIN : "/user/auth/login",
+    USER_AUTH_RESEND_OTP : "/user/auth/resend-otp",
+    USER_AUTH_VERIFY_OTP : "/user/auth/verify-otp",
+    USER_AUTH_FORGOT_PASSWORD_OTP : "/user/auth/forgot-password/otp",
+    USER_AUTH_FORGOT_PASSWORD_VERIFY_OTP : "/user/auth/forgot-password/verify-otp",
+    USER_AUTH_FORGOT_PASSWORD : "/user/auth/forgot-password",
+    USER_AUTH_LOGOUT : "/user/auth/logout",
+    USER_REFRESH_TOKEN : "/user/refresh-token",
+    USER_ME : "/user/me"
+}
